740c34893b55a1e361a1f76e40fdb43e38caa2e0
Sherwin Mina <sherwinmina@yahoo.com>
2017-04-27 15:23:34 -0700

    updated Userschema

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,38 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+
+var UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    require: true,
+    trim: true,
+    minLength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    }, 
+    token: {
+      access: {
+        typr: String,
+        required: true
+      }
+    }
+  }]
+});
 
 var User = mongoose.model('User', {
   email: {
